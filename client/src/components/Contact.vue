7397a0b50cd0296066eeecf4bfc859608ecc5550
Ashad <160583282+ashad9xtechnology@users.noreply.github.com>
2024-07-15 22:04:16 +0530

    added live url

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -31,11 +31,11 @@ export default {
   },
   methods: {
     async fetchContacts() {
-      const response = await axios.get('http://localhost:5000/api/contacts');
+      const response = await axios.get('https://contact-application-hnnd.onrender.com/api/contacts');
       this.contacts = response.data;
     },
     async addContact() {
-      await axios.post('http://localhost:5000/api/contacts', this.newContact);
+      await axios.post('https://contact-application-hnnd.onrender.com/api/contacts', this.newContact);
       this.newContact = { name: '', email: '', message: '' };
       this.fetchContacts();
     },
